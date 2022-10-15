48ab58a07e69e5b8044f79b56ff75f4860acb948
Alina-Halchenko <104688698+Alina-Halchenko@users.noreply.github.com>
2022-10-15 12:25:32 +0300

    1st task - done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+body: document.querySelector('body'),
+startBtn: document.querySelector('[data-start]'),
+stopBtn: document.querySelector('[data-stop]'),
+};
+let changeColorTimerId = null;
+let isActive = false;
+
+refs.startBtn.addEventListener('click', ()=> {
+if(isActive){
+  return
+};
+// console.log('times is on');
+changeColor();
+});
+
+function changeColor(){
+  changeColorTimerId = setInterval(() => {
+    isActive = true;
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+refs.stopBtn.addEventListener('click', ()=> {
+  clearInterval(changeColorTimerId);
+  isActive = false;
+})
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
